25201c797516acc286bb34e91b4c796ed95f1bf8
MMFields <95514396+ummf7@users.noreply.github.com>
2022-02-11 22:54:38 -0500

    Create Pytech_update.py

---
+++ Pytech_update.py
@@ -0,0 +1,15 @@
+<<<DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY>>>
+Student IO: 1007
+First Name: Thorin
+Last Name: Fields
+
+doc = db.collection_name.find_one({“student_id”: “1008”})
+ 
+ Student IO: 1008
+First Name: Thorin
+Last Name: Fields
+
+doc. = db.collection_name.delete_one({"student_id": "1009"})
+Student IO: 1009
+First Name: Thorin
+Last Name: Fields
